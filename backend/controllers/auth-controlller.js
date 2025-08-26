e42095b2090260d055035e60df10d0d04bc45b19
Phillip Vo <phillipvo321@gmail.com>
2025-08-26 14:20:33 +1000

    feat: implement controller to contain authentication request handling logic

---
+++ backend/controllers/auth-controlller.js
@@ -0,0 +1,17 @@
+const home = async (req, res) => {
+    try {
+        res.send("Welcome to the home page using controllers");
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const register = async (req, res) => {
+    try {
+        res.send("Welcome to the register page using controller");
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+module.exports = { home, register };
--- backend/router/auth-router.js
+++ backend/router/auth-router.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const { home, register } = require("../controllers/auth-controlller");
 
-router.route("/").get((req, res) => {
-    res.send("Hello World using Router");
-});
+// router.route("/").get((req, res) => {
+//     res.send("Hello World using Router");
+// });
+router.route("/").get(home);
+router.route("/register").post(register);
+router.route("/register").get(register);
 
 module.exports = router;
