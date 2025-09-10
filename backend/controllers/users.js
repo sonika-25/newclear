58f7ca74e5ec2a5bdcc48909cde1f0ff9c596194
Phillip Vo <phillipvo321@gmail.com>
2025-09-10 10:52:13 +1000

    fix: typo in User creation

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,36 +1,40 @@
 const User = require("../model/user-model");
-const express = require('express')
-const app = express()
-const router = require('express').Router()
+const express = require("express");
+const app = express();
+const router = require("express").Router();
 
-router.get("/", async (req,res) =>{
-    User.find({})
-        .then(data=>{
-            res.json(data)
-        })
-})
+router.get("/", async (req, res) => {
+    User.find({}).then((data) => {
+        res.json(data);
+    });
+});
 
-router.post ("/", async (req,res) => {
+router.post("/", async (req, res) => {
     const { username, email, phone, password } = req.body;
-     if (!username || !email || !phone || !password) {
-            // client error
-            return res
-                .status(400)
-                .json({ message: "Please fill all the fields" });
-        }
-        const userExists = await User.findOne({ email });
-        if (userExists) {
-            return res.status(400).json({ message: "User already exists" });
-        }
-        try {
-        const user = new Users({ username, email, phone, password, isAdmin });
+    if (!username || !email || !phone || !password) {
+        // client error
+        return res.status(400).json({ message: "Please fill all the fields" });
+    }
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+        return res.status(400).json({ message: "User already exists" });
+    }
+    try {
+        isAdmin = false;
+        const user = new User({
+            username,
+            role,
+            org_id,
+            email,
+            phone,
+            password,
+            isAdmin,
+        });
         await user.save();
-            res.status(201).json(user);
-        } catch (error) {
-            res.status(400).json({ error: error.message });
-        }
-
-
-})
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
 
 module.exports = router;
