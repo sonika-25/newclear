7439c211a27d04a8577f0a28ab06fa6a92fa86d0
draywoodcros <darcy.raywoodcross@student.unimelb.edu.au>
2025-09-23 15:46:01 +1000

    feat: added splitter and form to management page

--- frontend/src/components/UILayout.jsx
+++ frontend/src/components/UILayout.jsx
@@ -1,13 +1,14 @@
 import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./Navbar";
 import { Layout } from "antd";
+import { Content } from "antd/es/layout/layout";
 
 const UILayout = () => {
    return( 
    <Layout>
         <Navbar />
         <Layout style={{ minHeight: "100vh" }}>
-            <Outlet />
+                <Outlet />
         </Layout>
     </Layout>
     );
--- frontend/src/pages/ManagementPage.jsx
+++ frontend/src/pages/ManagementPage.jsx
@@ -1,25 +1,80 @@
-
-import {Layout, theme } from 'antd';
+import './css/management.css';
+import {Modal, Layout, List, Splitter, Button, Form, Input, Switch, message, Space, Typography } from 'antd';
 const { Content } = Layout;
-export default function ManagementPage() {
-    const {
-    token: { colorBgContainer, borderRadiusLG },
-    } = theme.useToken();
 
+  export default function ManagementPage() {
+
+  const [form] = Form.useForm();
+
+  const onFinish = (values) => {
+    
+  };
+
+  
     return (
+      
         <Layout>
-        <Content style={{ padding: '20px 20px' }}>
+        <Content className='manageContent' style={{padding: '10px 15px' }}>
           <div
             style={{
               background: "#FFFFFF",
-              padding: 50,
+              padding: 20,
               minHeight: "100%",
-              borderRadius: borderRadiusLG,
+              borderRadius: 20,
             }}
           >
-            Content
+              <Splitter style={{ height: "890px"}}>
+
+                <Splitter.Panel defaultSize="35%">
+                  <Form
+                    form={form}
+                    onFinish={onFinish}
+                    layout="vertical"
+                    autoComplete="off"
+                  >
+                    <Form.Item style={{marginRight: 20}}
+                      name="email"
+                      label="Enter User Email"
+                      rules={[{ required: true }, { type: 'email', warningOnly: true }]}
+                    >
+                      
+                      <Input placeholder="Enter user email" />
+                    </Form.Item>
+                    <Form.Item style={{marginRight: 20}}
+                      name="firstName"
+                      label="Enter User First Name"
+                      rules={[{ required: true }]}
+                    >
+                      <Input placeholder="Enter user first name" />
+                    </Form.Item>
+                     <Form.Item style={{marginRight: 20}}
+                      name="lastName"
+                      label="Enter User Last Name"
+                      rules={[{ required: true }]}
+                    >
+                      <Input placeholder="Enter user last name" />
+                    </Form.Item>
+                    <Form.Item name="admin" label="Enable Admin" valuePropName="checked" initialValue={false}>
+                    <Switch />
+                  </Form.Item>
+                    <Form.Item>
+                      <Space>
+                        <Button type="primary" htmlType="submit" block>
+                          Add
+                        </Button>
+                      </Space>
+                    </Form.Item>
+                </Form>
+                </Splitter.Panel>
+
+                <Splitter.Panel>
+                </Splitter.Panel>
+
+            </Splitter>
+           
           </div>
         </Content>
       </Layout>
+
     );
 }
