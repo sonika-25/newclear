e7153341ffd2758ea514c64e2b84ff951cac46a0
draywoodcros <darcy.raywoodcross@student.unimelb.edu.au>
2025-10-14 17:15:13 +1100

    feat: added upload list and button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,17 +5,21 @@ import HomePage from "./pages/HomePage";
 import SchedulePage from "./pages/SchedulePage";
 import ManagementPage from "./pages/ManagementPage";
 import SelectionPage from "./pages/SelectionPage";
+import EvidencePage from "./pages/EvidencePage";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />}/>
       <Route path="/select" element={<SelectionPage/>}/>
+
       <Route element={<UILayout/>}>
         <Route path="/home" element={<HomePage />}/>
         <Route path="/schedule" element={<SchedulePage/>}/>
         <Route path="/management" element={<ManagementPage/>}/>
+        <Route path="/evidence" element={<EvidencePage/>}/>
       </Route>
+
       <Route path="*" element={<Navigate to="/login" replace />}/>
     </Routes>
   );
--- frontend/src/pages/EvidencePage.jsx
+++ frontend/src/pages/EvidencePage.jsx
@@ -1,14 +1,67 @@
+import React, {useState} from "react"
+import {Layout, Card, Empty, Typography, Button, Upload} from 'antd';
+import { UploadOutlined } from "@ant-design/icons";
+
+/*Sections of this code utilises the ant design template structured provided on their website
+They are intended to used as a foundational starting point and are entirely open source
+Link to reference: https://ant.design/components/upload*/
+const { Content } = Layout;
+const {Dragger} = Upload;
+
+
 export default function EvidencePage() {
+  const [evidence, setEvidence] = useState([
+  {
+    uid: 'image1',
+    name: 'test.png',
+    status: 'uploading',
+    percent: 33,
+  },
+  {
+    uid: 'image2',
+    name: '.png',
+    status: 'error',
+  },
+]);
+  const isEmpty = evidence.length == 0;
+    const showEvidence = ({ fileList }) => setEvidence(fileList);
     return (
-        <div
-            style={{
-                minHeight: "100vh",
-                display: "grid",
-                placeItems: "center",
-                background: "#FFE3E3",
-                color: "#FFE3E3"
-            }}
-        >
-        </div>
+        <Layout>
+                <Content className='manageContent' style={{padding: '10px 15px' }} >
+                  <div
+                    style={{
+                      background: "#FFFFFF",
+                      padding: 20,
+                      minHeight: "100%",
+                      borderRadius: 20,
+                    }}
+                  >
+                      <Card style={{ background:"#6262620a",height: 890, display: "flex", flexDirection: "column" }}styles={{ body: { flex: 1, overflowY: "auto"} }}>
+                              {isEmpty ? (
+                              <Empty>
+                                <Upload action="" defaultFileList={evidence} showUploadList={false} onChange={showEvidence}>
+                                  <Button style={{height: 80, width: 130, marginBottom: 50, fontWeight: 500}}  color="primary" variant="dashed" type="primary" icon={<UploadOutlined />}>
+                                    Upload
+                                  </Button>
+                                </Upload>
+                                </Empty>
+                              ):(
+                              <Upload
+                                action="" //post to db inside action. just pass it a function
+                                listType="picture"
+                                defaultFileList={evidence}
+                              >
+                                <Button style={{height: 80, width: 130, marginBottom: 50, fontWeight: 500}}  color="primary" variant="dashed" type="primary" icon={<UploadOutlined />}>
+                                  Upload
+                                </Button>
+                              </Upload>   )}
+                                </Card>
+                     
+                    </div>
+
+                   
+
+                </Content>
+        </Layout>
     );
 }
